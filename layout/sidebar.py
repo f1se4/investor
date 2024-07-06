040ff5797d57f868606f8b0f6ed603b0701603d7
f1se4 <sergi.garcia.munoz@gmail.com>
2024-07-06 06:33:39 +0000

    investor 7

--- layout/sidebar.py
+++ layout/sidebar.py
@@ -55,10 +55,10 @@ def configure_sidebar():
     end_time = st.sidebar.date_input("Fecha Final",
                                      datetime.datetime.today(),
                                      format="DD/MM/YYYY")
-    st.sidebar.markdown('<a href="https://www.fisoft.es/" target="_blank" class="link-text">By: www.fisoft.es</a>', unsafe_allow_html=True)
     st.sidebar.header("Help")
     st.sidebar.image(Image.open('assets/velas.jpg'))
 
+    st.sidebar.markdown('<a href="https://www.fisoft.es/" target="_blank" class="link-text">By: www.fisoft.es</a>', unsafe_allow_html=True)
 
     return stock, start_time, end_time
 
