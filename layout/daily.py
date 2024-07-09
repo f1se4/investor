316a02ba1e65e2a46805e674b357688f93f9f34f
f1se4 <sergi.garcia.munoz@gmail.com>
2024-07-09 12:28:19 +0200

    production

--- layout/daily.py
+++ layout/daily.py
@@ -11,5 +11,9 @@ def daily(ticker_data, selected_interval):
     utc_tz = utc
     cest_tz = timezone('Europe/Paris')  # Assuming CEST is Europe/Paris time zone
     daily_data = yf.download(ticker_data.get_info()['symbol'],period='1d',interval=selected_interval)
+
+    # # Convert index (date) to CEST
+    # daily_data.index = daily_data.index.tz_localize(utc_tz).tz_convert(cest_tz)
+
     plot_daily = plot_candlestick_daily(daily_data)
     st.plotly_chart(plot_daily)
