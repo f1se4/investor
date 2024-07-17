19ef596b9ed13449b77cd21107b8f482cc6d5aa6
f1se4 <sergi.garcia.munoz@gmail.com>
2024-07-17 16:42:25 +0200

    production

--- app.py
+++ app.py
@@ -124,7 +124,6 @@ def main():
             data = bot.generate_signals(data, show_g_strategy, show_trade_simple, show_MM)
             st.plotly_chart(bot.plot_data(data.tail(values), ticker, show_g_strategy, show_trade_simple, show_MM))
             backtest = bot.style_dataframe(bot.f_backtesting(data))
-            st.write(f'{backtest}')
             st.dataframe(backtest, use_container_width=True)
             # except:
             #     st.write(f"Errors loading {ticker}")
