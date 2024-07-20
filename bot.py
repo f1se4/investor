e8098c575c03803a7cd21992088cbaebf0e16820
f1se4 <sergi.garcia.munoz@gmail.com>
2024-07-20 11:22:38 +0200

    production

--- bot.py
+++ bot.py
@@ -495,3 +495,153 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
 #         subset=['Return'])
 #
 #     return styled_df
+
+def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI, show_par=True):
+    # Asegurar que la columna 'Datetime' esté en formato datetime
+    data['Datetime'] = pd.to_datetime(data['Datetime'])
+    
+    format = '%Y-%m-%d %H:%M'
+    company_name = get_company_name(ticker)
+    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, 
+                        row_heights=[0.7, 0.1, 0.1, 0.1],
+                        vertical_spacing=0.05)
+
+    # Añadir gráfico de velas (candlestick)
+    fig.add_trace(go.Candlestick(x=data.index,
+                                 open=data['Open'],
+                                 high=data['High'],
+                                 low=data['Low'],
+                                 close=data['Close'],
+                                 name='Candlestick'), row=1, col=1)
+
+    if show_g_channel:
+        # Calcular POC, VAL y VAH
+        poc_price, val, vah = calculate_poc_val_vah(data)
+        
+        # Identificar otros máximos relativos en el volumen
+        volume_peaks = data['Volume'][(data['Volume'].shift(2) < data['Volume']) & (data['Volume'].shift(-2) < data['Volume'])]
+
+        # Marcar el POC, VAL y VAH en el gráfico de precios
+        fig.add_trace(go.Scatter(
+            x=[data.index[0], data.index[-1]],
+            y=[poc_price, poc_price],
+            mode='lines',
+            name='POC',
+            line=dict(color='rgba(68,102,119,0.8)', dash='dash')
+        ))
+        
+        fig.add_trace(go.Scatter(
+            x=[data.index[0], data.index[-1]],
+            y=[val, val],
+            mode='lines',
+            name='VAL',
+            line=dict(color='rgba(107,107,107,0.5)', dash='dash'))
+        )
+        
+        fig.add_trace(go.Scatter(
+            x=[data.index[0], data.index[-1]],
+            y=[vah, vah],
+            mode='lines',
+            name='VAH',
+            line=dict(color='rgba(107,107,107,0.5)', dash='dash')
+        ))
+
+        # Marcar otros máximos relativos
+        peak_lines = []
+        for peak_date, peak_volume in volume_peaks.items():
+            peak_price = data.loc[peak_date, 'Close']
+            peak_lines.append(fig.add_trace(go.Scatter(
+                y=[peak_price, peak_price],
+                x=[data.index[0], data.index[-1]],
+                mode='lines',
+                name=f'Peak {peak_date.date()}',
+                line=dict(color='rgba(93,93,93,0.1)', dash='dot')
+            )))
+
+    if show_par:
+        fig.add_trace(go.Scatter(x=data.index, y=data['SAR'],
+                             mode='markers',
+                             marker=dict(color='rgba(125,132,113,0.4)', size=5),
+                             name='Parabolic SAR'))
+    
+
+    fig.add_trace(go.Bar(x=data.index, y=data.MACD, 
+                         marker_color=np.where(data.MACD >= 0, 'green', 'darkgray'), 
+                         opacity=0.6), row=3, col=1)
+
+    # Añadir gráfico de volumen al segundo subplot
+    fig.add_trace(go.Bar(x=data.index, y=data['Volume'], name='Volume', marker_color='rgba(31, 119, 180, 0.3)'),
+                  row=2, col=1)
+    # Añadir gráfico de volumen al segundo subplot
+    fig.add_trace(go.Scatter(x=data.index, y=data['Volume_Avg'], name='Volume', 
+                             marker_color='rgba(131, 119, 180, 0.4)'),
+                  row=2, col=1)
+    fig.add_trace(go.Scatter(x=data.index, y=data['RSI'], name='RSI', 
+                             marker_color='rgba(131, 119, 180, 0.6)'),
+                  row=4, col=1)
+    fig.add_trace(go.Scatter(
+        mode='lines',
+                y=[30, 30],
+                x=[data.index[0], data.index[-1]],
+                             marker_color='rgba(239,169,74,0.3)'),
+                  row=4, col=1)
+    fig.add_trace(go.Scatter(
+                mode='lines',
+                y=[70, 70],
+                x=[data.index[0], data.index[-1]],
+                marker_color='rgba(239,169,74,0.3)'),
+                row=4, col=1)
+
+    divergences = identify_rsi_divergences(data)
+    for divergence in divergences:
+        if divergence[2] == 'Bullish':
+            fig.add_trace(go.Scatter(x=[divergence[0]], y=[divergence[1]], mode='markers', 
+                                     marker=dict(color='rgba(0,255,0,0.4)', size=8, symbol='arrow-up'), 
+                                     name='Bullish'),row=4,col=1)
+        if divergence[2] == 'Bearish':
+            fig.add_trace(go.Scatter(x=[divergence[0]], y=[divergence[1]], 
+                                     mode='markers', marker=dict(color='rgba(255,0,0,0.4)', size=8, symbol='arrow-down'), 
+                                     name='Bearish'),row=4,col=1)
+    
+    if show_simple_trade:
+        fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_High'], mode='lines', name='Bollinger High',
+                                 line=dict(color='rgba(248, 237, 98, 0.3)')))
+
+        fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_Low'], mode='lines', name='Bollinger Low',
+                                 line=dict(color='rgba(233,215,0, 0.3)')))
+
+        fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_High'], fill='tonexty',
+                                 fillcolor='rgba(248,237,98, 0.1)', line=dict(color='rgba(248, 237, 98, 0.3)'),
+                                 mode='lines', name='Bollinger Bands'))
+        fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_Low'], fill='tonexty',
+                                 fillcolor='rgba(248,237,98, 0.1)', line=dict(color='rgba(214,39,40, 0.3)'),
+                                 mode='lines', name='Bollinger Bands'))
+
+    if show_MM:
+        fig.add_trace(go.Scatter(x=data.index, y=data['SMAII'], mode='lines', name='SMA II',
+                              line=dict(color='rgba(153,204,255, 0.8)')))
+    if show_MMI:
+        fig.add_trace(go.Scatter(x=data.index, y=data['SMAI'], mode='lines', name='SMA I', line=dict(color='rgba(85,136,255,0.8)', width=1)))
+
+    # Encontrar días faltantes
+    alldays = set(data['Datetime'].min() + timedelta(days=x) for x in range((data['Datetime'].max() - data['Datetime'].min()).days + 1))
+    missing = sorted(set(alldays) - set(data['Datetime']))
+
+    # Configurar los rangebreaks en los ejes x
+    fig.update_xaxes(rangebreaks=[dict(values=missing)])
+
+    fig.update_layout(title=f'{ticker} - {company_name}', 
+                      xaxis_title='', yaxis_title='',
+                      yaxis=dict(
+                                gridcolor='rgba(200, 200, 200, 0.03)',  # Color de la cuadrícula con transparencia
+                                gridwidth=1  # Anchura de la cuadrícula
+                        ),
+                      xaxis_rangeslider_visible=False,
+                      height=700,
+                      dragmode='drawline',  # Habilitar el modo de dibujo de líneas
+                      shapes=[],  # Inicializar lista vacía para las líneas dibujadas
+                      newshape=dict(line=dict(color="red")),
+                      modebar_add=['drawline','eraseshape'],
+                      showlegend=False)
+
+    return fig
