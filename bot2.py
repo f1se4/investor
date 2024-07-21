1428e88377900aaaf930c051479ee5c324d19029
f1se4 <sergi.garcia.munoz@gmail.com>
2024-07-21 08:25:38 +0200

    production

--- bot2.py
+++ bot2.py
@@ -8,11 +8,23 @@ from calculations.calculations import get_company_name
 from datetime import timedelta
 
 def f_parabolic_SAR(df, af=0.03, max_af=0.3):
+    """
+    Calcula el Parabolic SAR para un dataframe de pandas con columnas 'High' y 'Low'.
+
+    Parameters:
+    df (pandas.DataFrame): DataFrame con los datos de precios.
+    af (float): Factor de aceleración inicial.
+    max_af (float): Factor de aceleración máximo.
+
+    Returns:
+    pandas.DataFrame: DataFrame con una columna adicional 'SAR' con los valores del Parabolic SAR.
+    """
     high = df['High']
     low = df['Low']
     
     sar = df['Close'].copy()
     uptrend = True
+    af = af
     ep = high.iloc[0]
     
     for i in range(1, len(df)):
@@ -43,9 +55,11 @@ def f_parabolic_SAR(df, af=0.03, max_af=0.3):
     df['SAR'] = sar
     return df
 
+# Función para calcular la EMA
 def ema(series, window):
     return series.ewm(span=window, adjust=False).mean()
 
+# Función para calcular el RSI
 def rsi(series, window):
     delta = series.diff()
     gain = (delta.where(delta > 0, 0)).rolling(window=window,  min_periods=1).mean()
@@ -53,26 +67,18 @@ def rsi(series, window):
     rs = gain / loss
     return 100 - (100 / (1 + rs))
 
-def identify_rsi_divergences(df, lbL=5, lbR=5, range_upper=60, range_lower=5):
+# Identificar divergencias
+def identify_rsi_divergences(df):
     divergences = []
-    df['PivotLow'] = df['RSI'].rolling(window=lbL + lbR + 1, center=True).apply(lambda x: x[lbL] if x[lbL] == min(x) else np.nan, raw=True)
-    df['PivotHigh'] = df['RSI'].rolling(window=lbL + lbR + 1, center=True).apply(lambda x: x[lbL] if x[lbL] == max(x) else np.nan, raw=True)
-    
-    for i in range(len(df)):
-        if not np.isnan(df['PivotLow'].iloc[i]):
-            for j in range(i + 1, min(i + range_upper, len(df))):
-                if not np.isnan(df['PivotLow'].iloc[j]) and df['Low'].iloc[j] > df['Low'].iloc[i] and df['RSI'].iloc[j] < df['RSI'].iloc[i]:
-                    divergences.append((df.index[j], df['RSI'].iloc[j], 'Bullish'))
-                if not np.isnan(df['PivotLow'].iloc[j]) and df['Low'].iloc[j] < df['Low'].iloc[i] and df['RSI'].iloc[j] > df['RSI'].iloc[i]:
-                    divergences.append((df.index[j], df['RSI'].iloc[j], 'Hidden Bullish'))
-        
-        if not np.isnan(df['PivotHigh'].iloc[i]):
-            for j in range(i + 1, min(i + range_upper, len(df))):
-                if not np.isnan(df['PivotHigh'].iloc[j]) and df['High'].iloc[j] < df['High'].iloc[i] and df['RSI'].iloc[j] > df['RSI'].iloc[i]:
-                    divergences.append((df.index[j], df['RSI'].iloc[j], 'Bearish'))
-                if not np.isnan(df['PivotHigh'].iloc[j]) and df['High'].iloc[j] > df['High'].iloc[i] and df['RSI'].iloc[j] < df['RSI'].iloc[i]:
-                    divergences.append((df.index[j], df['RSI'].iloc[j], 'Hidden Bearish'))
-                    
+    for i in range(1, len(df) - 1):
+        # Divergencia Alcista: Precio haciendo un mínimo más bajo y RSI haciendo un mínimo más alto
+        if df['Close'][i] < df['Close'][i-1] and df['Close'][i] < df['Close'][i+1]:
+            if df['RSI'][i] > df['RSI'][i-1] and df['RSI'][i] > df['RSI'][i+1]:
+                divergences.append((df.index[i], df['RSI'][i], 'Bullish'))
+        # Divergencia Bajista: Precio haciendo un máximo más alto y RSI haciendo un máximo más bajo
+        if df['Close'][i] > df['Close'][i-1] and df['Close'][i] > df['Close'][i+1]:
+            if df['RSI'][i] < df['RSI'][i-1] and df['RSI'][i] < df['RSI'][i+1]:
+                divergences.append((df.index[i], df['RSI'][i], 'Bearish'))
     return divergences
 
 def calculate_macd(df):
@@ -83,6 +89,7 @@ def calculate_macd(df):
     df['MACD'] = df['MACD_Line'] - df['Signal_Line']
     return df
 
+# Función para calcular las bandas de Bollinger
 def bollinger_bands(series, window):
     sma = series.rolling(window=window).mean()
     std = series.rolling(window=window).std()
@@ -90,6 +97,7 @@ def bollinger_bands(series, window):
     lower_band = sma - (std * 2)
     return upper_band, lower_band
 
+# Función para obtener los datos históricos
 def get_data(ticker, selected_interval, select_period, smai=200, smaii=100):
     data = yf.download(ticker, period=select_period, interval=selected_interval)
     try:
@@ -117,60 +125,30 @@ def get_data(ticker, selected_interval, select_period, smai=200, smaii=100):
 
     return data
 
-def determine_action(data, position):
-    if position == 'None':
-        if data.iloc[-1]['Buy'] >= 1:
-            return 'Buy', data.index[-1], data.index[-1]['Close']
-        else:
-            return 'Hold', None, None
-    elif position == 'Long':
-        if data.iloc[-1]['Sell'] >= 1:
-            return 'Sell', data.index[-1], data.index[-1]['Close']
-        else:
-            return 'Hold', None, None
-
-def update_portfolio(ticker, action, quantity, price):
-    portfolio_file = 'portfolio.csv'
-    
-    if not os.path.exists(portfolio_file):
-        df = pd.DataFrame(columns=['Ticker', 'Action', 'Quantity', 'Price', 'Date'])
-    else:
-        df = pd.read_csv(portfolio_file)
-    
-    new_trade = {
-        'Ticker': ticker,
-        'Action': action,
-        'Quantity': quantity,
-        'Price': price,
-        'Date': pd.Timestamp.now()
-    }
-    df = df.append(new_trade, ignore_index=True)
-    df.to_csv(portfolio_file, index=False)
-    return df
-
-def show_portfolio():
-    portfolio_file = 'portfolio.csv'
-    
-    if os.path.exists(portfolio_file):
-        df = pd.read_csv(portfolio_file)
-        return df
-    else:
-        return pd.DataFrame(columns=['Ticker', 'Action', 'Quantity', 'Price', 'Date'])
-
 def calculate_poc_val_vah(data):
+    # Crear una DataFrame con precios y volúmenes
     price_volume_df = data[['Close', 'Volume']].copy()
+    
+    # Calcular el POC (precio con mayor volumen)
     poc_index = data['Volume'].idxmax()
     poc_price = data.loc[poc_index, 'Close']
     
+    # Ordenar por precios
     price_volume_df = price_volume_df.sort_values(by='Close')
+    
+    # Calcular el volumen total
     total_volume = price_volume_df['Volume'].sum()
+    
+    # Calcular el volumen objetivo (70% del volumen total)
     target_volume = total_volume * 0.70
     
+    # Inicializar variables para encontrar VAL y VAH
     cumulative_volume = 0
     val = None
     vah = None
     middle_price = poc_price
 
+    # Encuentra VAL (Value Area Low) y VAH (Value Area High) alrededor del POC
     lower_half_volume = price_volume_df[price_volume_df['Close'] <= middle_price]
     upper_half_volume = price_volume_df[price_volume_df['Close'] > middle_price]
 
@@ -178,6 +156,7 @@ def calculate_poc_val_vah(data):
     upper_cumulative_volume = upper_half_volume['Volume'].sum()
 
     if lower_cumulative_volume > target_volume / 2:
+        # Encuentra el VAL en la mitad inferior
         lower_half_volume = lower_half_volume.sort_values(by='Close', ascending=False)
         cumulative_volume = 0
         for price, volume in lower_half_volume.itertuples(index=False):
@@ -189,6 +168,7 @@ def calculate_poc_val_vah(data):
         val = lower_half_volume['Close'].min()
 
     if upper_cumulative_volume > target_volume / 2:
+        # Encuentra el VAH en la mitad superior
         upper_half_volume = upper_half_volume.sort_values(by='Close')
         cumulative_volume = 0
         for price, volume in upper_half_volume.itertuples(index=False):
@@ -202,6 +182,7 @@ def calculate_poc_val_vah(data):
     return poc_price, val, vah
 
 def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI, show_par=True):
+    # Asegurar que la columna 'Datetime' esté en formato datetime
     data['Datetime'] = pd.to_datetime(data['Datetime'])
     
     format = '%Y-%m-%d %H:%M'
@@ -210,6 +191,7 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
                         row_heights=[0.7, 0.1, 0.1, 0.1],
                         vertical_spacing=0.05)
 
+    # Añadir gráfico de velas (candlestick)
     fig.add_trace(go.Candlestick(x=data.index,
                                  open=data['Open'],
                                  high=data['High'],
@@ -218,9 +200,13 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
                                  name='Candlestick'), row=1, col=1)
 
     if show_g_channel:
+        # Calcular POC, VAL y VAH
         poc_price, val, vah = calculate_poc_val_vah(data)
+        
+        # Identificar otros máximos relativos en el volumen
         volume_peaks = data['Volume'][(data['Volume'].shift(2) < data['Volume']) & (data['Volume'].shift(-2) < data['Volume'])]
 
+        # Marcar el POC, VAL y VAH en el gráfico de precios
         fig.add_trace(go.Scatter(
             x=[data.index[0], data.index[-1]],
             y=[poc_price, poc_price],
@@ -245,6 +231,7 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
             line=dict(color='rgba(107,107,107,0.5)', dash='dash')
         ))
 
+        # Marcar otros máximos relativos
         peak_lines = []
         for peak_date, peak_volume in volume_peaks.items():
             peak_price = data.loc[peak_date, 'Close']
@@ -262,12 +249,15 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
                              marker=dict(color='rgba(125,132,113,0.4)', size=5),
                              name='Parabolic SAR'))
     
+
     fig.add_trace(go.Bar(x=data.index, y=data.MACD, 
                          marker_color=np.where(data.MACD >= 0, 'green', 'darkgray'), 
                          opacity=0.6), row=3, col=1)
 
+    # Añadir gráfico de volumen al segundo subplot
     fig.add_trace(go.Bar(x=data.index, y=data['Volume'], name='Volume', marker_color='rgba(31, 119, 180, 0.3)'),
                   row=2, col=1)
+    # Añadir gráfico de volumen al segundo subplot
     fig.add_trace(go.Scatter(x=data.index, y=data['Volume_Avg'], name='Volume', 
                              marker_color='rgba(131, 119, 180, 0.4)'),
                   row=2, col=1)
@@ -297,14 +287,6 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
             fig.add_trace(go.Scatter(x=[divergence[0]], y=[divergence[1]], 
                                      mode='markers', marker=dict(color='rgba(255,0,0,0.4)', size=8, symbol='arrow-down'), 
                                      name='Bearish'),row=4,col=1)
-        if divergence[2] == 'Hidden Bullish':
-            fig.add_trace(go.Scatter(x=[divergence[0]], y=[divergence[1]], mode='markers', 
-                                     marker=dict(color='rgba(0,150,0,0.4)', size=8, symbol='arrow-up'), 
-                                     name='Hidden Bullish'),row=4,col=1)
-        if divergence[2] == 'Hidden Bearish':
-            fig.add_trace(go.Scatter(x=[divergence[0]], y=[divergence[1]], 
-                                     mode='markers', marker=dict(color='rgba(150,0,0,0.4)', size=8, symbol='arrow-down'), 
-                                     name='Hidden Bearish'),row=4,col=1)
     
     if show_simple_trade:
         fig.add_trace(go.Scatter(x=data.index, y=data['Bollinger_High'], mode='lines', name='Bollinger High',
@@ -326,21 +308,23 @@ def plot_data(data, ticker, show_g_channel, show_simple_trade, show_MM, show_MMI
     if show_MMI:
         fig.add_trace(go.Scatter(x=data.index, y=data['SMAI'], mode='lines', name='SMA I', line=dict(color='rgba(85,136,255,0.8)', width=1)))
 
+    # Encontrar días faltantes
     alldays = set(data['Datetime'].min() + timedelta(days=x) for x in range((data['Datetime'].max() - data['Datetime'].min()).days + 1))
     missing = sorted(set(alldays) - set(data['Datetime']))
 
+    # Configurar los rangebreaks en los ejes x
     fig.update_xaxes(rangebreaks=[dict(values=missing)])
 
     fig.update_layout(title=f'{ticker} - {company_name}', 
                       xaxis_title='', yaxis_title='',
                       yaxis=dict(
-                                gridcolor='rgba(200, 200, 200, 0.03)',
-                                gridwidth=1
+                                gridcolor='rgba(200, 200, 200, 0.03)',  # Color de la cuadrícula con transparencia
+                                gridwidth=1  # Anchura de la cuadrícula
                         ),
                       xaxis_rangeslider_visible=False,
                       height=700,
-                      dragmode='drawline',
-                      shapes=[],
+                      dragmode='drawline',  # Habilitar el modo de dibujo de líneas
+                      shapes=[],  # Inicializar lista vacía para las líneas dibujadas
                       newshape=dict(line=dict(color="red")),
                       modebar_add=['drawline','eraseshape'],
                       showlegend=False)
